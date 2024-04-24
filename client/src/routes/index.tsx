d5547a46546f76c71241113bf2e4de0673babeee
Dominik Sieroń <domin.powtorki@gmail.com>
2024-04-25 01:43:05 +0200

    Fix: bugfix

--- client/src/routes/index.tsx
+++ client/src/routes/index.tsx
@@ -1,5 +1,5 @@
 import { createFileRoute } from "@tanstack/react-router";
-import RecipiesForm from "@/components/RecipesForm";
+import RecipesForm from "@/components/RecipesForm";
 
 export const Route = createFileRoute("/")({
   component: () => <Home />,
@@ -9,7 +9,7 @@ const Home = () => {
   return (
     <>
       <div className="flex min-h-screen items-center justify-center">
-        <RecipiesForm />
+        <RecipesForm />
       </div>
     </>
   );
